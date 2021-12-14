cb9c9827b9c99b669677da2064cf38c658bdffa9
Thevic16 <20171408@ce.pucmm.edu.do>
2021-12-14 14:20:09 -0400

    training new model

--- Step2-Training/Training.py
+++ Step2-Training/Training.py
@@ -36,10 +36,10 @@ print('Total Validation Images: ',len(xVal))
 model = createModel()
 
 #### STEP 8 - TRAINNING
-history = model.fit(dataGen(xTrain, yTrain, 80, 1),
+history = model.fit(dataGen(xTrain, yTrain, 280, 1),
                                   steps_per_epoch=100,
                                   epochs=10,
-                                  validation_data=dataGen(xVal, yVal, 40, 0),
+                                  validation_data=dataGen(xVal, yVal, 140, 0),
                                   validation_steps=50)
 
 
